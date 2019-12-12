dcf829987ee51428ff1e53222f86dd4b4e21dbfa
Lightras <o.v.rabota@gmail.com>
2019-12-12 23:51:18 +0200

    add main.ts

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ let win;
 function createWindow() {
    // Create the browser window.
    win = new BrowserWindow({
-      height: 670,
+      height: 700,
       icon: `file://${__dirname}/dist/assets/logo.png`,
-      width: 800,
+      width: 1200,
       webPreferences: {
          webSecurity: false
       }
@@ -17,7 +17,7 @@ function createWindow() {
    // win.loadURL(`http://localhost:4200/`);
 
    // uncomment below to open the DevTools.
-   win.webContents.openDevTools();
+   // win.webContents.openDevTools();
    win.setMenuBarVisibility(false);
 
    // Event when the window is closed.
